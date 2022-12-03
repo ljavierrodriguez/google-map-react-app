f354ff7a785f5c17e71096799904dadf7d8851d5
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2022-12-03 18:52:51 +0000

    fixin makers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import L from 'leaflet';
 
 const App = () => {
 
+  const [markers, setMarkers] = useState([
+    { lat: 51.5, long: -0.09 },
+    { lat: 51.4, long: -0.08 },
+    { lat: 51.3, long: -0.07 }
+  ]);
+
   let map = useRef();
 
   useEffect(() => {
@@ -15,21 +21,24 @@ const App = () => {
   }, [])
 
   const loadMap = () => {
-    
+
     map = L.map('map', {
       center: [51.505, -0.09],
-      zoom: 8
+      zoom: 13
     });
-  
-    
+
+
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
+      maxZoom: 20,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    let marker = L.marker([51.5, -0.09]).addTo(map);
-    let marker2 = L.marker([31.5, -0.21]).addTo(map);
-  
+
+    markers.forEach((marker) => {
+      L.marker([marker.lat, marker.long]).addTo(map);
+    });
   }
+
+
 
   return (
     <div id="map" ref={t => map = t}></div>
